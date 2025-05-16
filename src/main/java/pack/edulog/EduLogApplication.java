500e8e737e12ac6965738eb2363faa8354bc368d
MOUGHIT <abdelmoughith2002@gmail.com>
2025-05-17 00:10:19 +0100

    start

---
+++ src/main/java/pack/edulog/EduLogApplication.java
@@ -0,0 +1,13 @@
+package pack.edulog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduLogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EduLogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pack/edulog/EduLogApplicationTests.java
@@ -0,0 +1,13 @@
+package pack.edulog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduLogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
